229a1588ad34535705360f2dd009143b9ad8f929
Nick Florez <128880858+NicoFJ09@users.noreply.github.com>
2025-09-23 13:28:37 -0600

    Feat(Utils): Created base files

---
+++ src/utils/board.rkt
@@ -0,0 +1,5 @@
+#lang racket
+;; board.rkt
+;; Placeholder for board-related utilities.
+;; Functionality will be implemented once the UI matrix data structure exists
+;; and can serve as a reference for creating board utils.
